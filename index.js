6602fed14a35be9e0be872baa755530b9356d823
mk-saadi <mksaadi820@gmail.com>
2024-06-29 21:05:17 +0600

    new updates

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 require("dotenv").config();
 const app = express();
@@ -8,128 +10,284 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+	const authorization = req.headers.authorization;
+	if (!authorization) {
+		return res.status(401).send({ error: true, message: "unauthorized access" });
+	}
+	const token = authorization.split(" ")[1];
+
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+		if (err) {
+			return res.status(401).send({ error: true, message: "unauthorized access" });
+		}
+		req.decoded = decoded;
+		next();
+	});
+};
+
 // mongoDB
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mrt0xqs.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `${process.env.Mongo_URI}`;
 
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 });
 
 async function run() {
-    try {
-        const figureCollection = client.db("animeFig").collection("figures");
-        const addedFigureCollection = client.db("animeFig").collection("addedFigure");
+	try {
+		const figureCollection = client.db("animeFig").collection("figures");
+		const addedFigureCollection = client.db("animeFig").collection("addedFigure");
+		const usersCollection = client.db("animeFig").collection("users");
 
-        app.get("/figures", async (req, res) => {
-            const cursor = figureCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        });
+		app.post("/jwt", (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+				expiresIn: "7d",
+			});
 
-        app.get("/figures/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await figureCollection.findOne(query);
-            res.send(result);
-        });
+			res.send({ token });
+		});
 
-        app.post("/figures", async (req, res) => {
-            const figures = req.body;
-            const result = await figureCollection.insertOne(figures);
-            res.send(result);
-        });
+		const verifyAdmin = async (req, res, next) => {
+			const email = req.decoded.email;
+			const query = { email: email };
+			const user = await usersCollection.findOne(query);
+			if (user?.role !== "admin") {
+				return res.status(403).send({ error: true, message: "forbidden access!" });
+			}
+			next();
+		};
 
-        app.get("/addedFigure", async (req, res) => {
-            let query = {};
+		// >> users api
+		app.get("/users", async (req, res) => {
+			let query = {};
 
-            if (req.query?.email) {
-                query = { email: req.query.email };
-            }
+			if (req.query?.email) {
+				query.email = {
+					$regex: req.query.email,
+					$options: "i",
+				};
+			}
 
-            if (req.query.search) {
-                query.name = { $regex: req.query.search, $options: "i" };
-            }
+			if (req.query?.name) {
+				query.name = { $regex: req.query.name, $options: "i" };
+			}
 
-            if (req.params.category) {
-                if (
-                    req.params.category == "Scale Figures" ||
-                    req.params.category == "Bishoujo Figures" ||
-                    req.params.category == "Figma" ||
-                    req.params.category == "Nendoroid"
-                ) {
-                    query.category = req.params.category;
-                } else {
-                    query.category = { $regex: req.params.category, $options: "i" };
-                }
-            }
+			if (req.query?.role) {
+				query.role = { $regex: req.query.role, $options: "i" };
+			}
 
-            // const limit = parseInt(req.query.limit) || 20;     .limit(limit)
+			const result = await usersCollection.find(query).toArray();
+			res.send(result);
+		});
 
-            const result = await addedFigureCollection.find(query).sort({ price: -1 }).toArray();
+		app.get("/users/:role/:email", verifyJWT, async (req, res) => {
+			const role = req.params.role;
+			const email = req.params.email;
 
-            res.send(result);
-        });
+			if (email !== req.decoded.email) {
+				return res.send({ [role]: false });
+			}
 
-        app.get("/addedFigure/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await addedFigureCollection.findOne(query);
-            res.send(result);
-        });
+			const query = { email: email };
+			const user = await usersCollection.findOne(query);
+			const result = { [role]: user?.role === role };
+			res.send(result);
+		});
 
-        app.post("/addedFigure", async (req, res) => {
-            const figure = req.body;
-            const result = await addedFigureCollection.insertOne(figure);
-            res.send(result);
-        });
+		app.patch("/users/:email", verifyJWT, verifyAdmin, async (req, res) => {
+			const { email } = req.params;
+			const { role } = req.body;
 
-        app.put("/addedFigure/:id", async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-            const options = { upsert: true };
+			try {
+				await usersCollection.updateOne({ email: email }, { $set: { role: role } });
 
-            const updatedFigs = req.body;
-            const figures = {
-                $set: {
-                    img: updatedFigs.img,
-                    email: updatedFigs.email,
-                    name: updatedFigs.name,
-                    price: updatedFigs.price,
-                    quantity: updatedFigs.quantity,
-                    seller: updatedFigs.seller,
-                    description: updatedFigs.description,
-                    Manufacturer: updatedFigs.Manufacturer,
-                    category: updatedFigs.category,
-                    rating: updatedFigs.rating,
-                },
-            };
-            const result = await addedFigureCollection.updateOne(filter, figures, options);
-            res.send(result);
-        });
+				res.status(200).send({
+					message: "User role updated successfully",
+				});
+			} catch (error) {
+				res.status(500).send({
+					error: "Internal server error",
+				});
+			}
+		});
 
-        app.delete("/addedFigure/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await addedFigureCollection.deleteOne(query);
-            res.send(result);
-        });
+		app.post("/users", async (req, res) => {
+			const user = req.body;
+			const query = { email: user.email };
+			const existingUser = await usersCollection.findOne(query);
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-    }
+			if (existingUser) {
+				return res.send({ message: "user already exists" });
+			}
+
+			const result = await usersCollection.insertOne(user);
+			res.send(result);
+		});
+
+		app.delete("/users/:id", verifyJWT, async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await usersCollection.deleteOne(query);
+			res.send(result);
+		});
+
+		// >> figures api
+		app.get("/figures", async (req, res) => {
+			const cursor = figureCollection.find();
+			const result = await cursor.toArray();
+			res.send(result);
+		});
+
+		app.get("/figures/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await figureCollection.findOne(query);
+			res.send(result);
+		});
+
+		app.post("/figures", async (req, res) => {
+			const figures = req.body;
+			const result = await figureCollection.insertOne(figures);
+			res.send(result);
+		});
+
+		// >> addedFigure api below
+		app.get("/addedFigure/pagination", async (req, res) => {
+			const page = parseInt(req.query.page) || 0;
+			const limit = parseInt(req.query.limit) || 6;
+			const skip = page * limit;
+			const result = await addedFigureCollection
+				.find(
+					{},
+					{
+						projection: {
+							_id: 1,
+							img: 1,
+							name: 1,
+							price: 1,
+							category: 1,
+							rating: 1,
+						},
+					}
+				)
+				.skip(skip)
+				.limit(limit)
+				.toArray();
+			res.send(result);
+		});
+
+		app.get("/totalAddedFigure", async (req, res) => {
+			const result = await addedFigureCollection.estimatedDocumentCount();
+			res.send({ totalAddedFigure: result });
+		});
+
+		app.post("/addedFigureById", async (req, res) => {
+			const ids = req.body;
+			const objectIds = ids.map((id) => new ObjectId(id));
+			const query = { _id: { $in: objectIds } };
+			const result = await addedFigureCollection.find(query).toArray();
+			res.send(result);
+		});
+		// pagination api above
+
+		app.get("/addedFigure", async (req, res) => {
+			let query = {};
+
+			if (req.query?.email) {
+				query = { email: req.query.email };
+			}
+
+			if (req.query.search) {
+				query.name = { $regex: req.query.search, $options: "i" };
+			}
+
+			if (req.params.category) {
+				if (
+					req.params.category == "Scale Figures" ||
+					req.params.category == "Bishoujo Figures" ||
+					req.params.category == "Figma" ||
+					req.params.category == "Nendoroid"
+				) {
+					query.category = req.params.category;
+				} else {
+					query.category = { $regex: req.params.category, $options: "i" };
+				}
+			}
+
+			// const limit = parseInt(req.query.limit) || 20;     .limit(limit)
+
+			const result = await addedFigureCollection.find(query).sort({ price: -1 }).toArray();
+
+			res.send(result);
+		});
+
+		app.get("/addedFigure/latest", async (req, res) => {
+			const cursor = addedFigureCollection.find().sort({ _id: -1 }).limit(5);
+			const result = await cursor.toArray();
+			res.send(result);
+		});
+
+		app.get("/addedFigure/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await addedFigureCollection.findOne(query);
+			res.send(result);
+		});
+
+		app.post("/addedFigure", async (req, res) => {
+			const figure = req.body;
+			const result = await addedFigureCollection.insertOne(figure);
+			res.send(result);
+		});
+
+		app.put("/addedFigure/:id", async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: new ObjectId(id) };
+			const options = { upsert: true };
+
+			const updatedFigs = req.body;
+			const figures = {
+				$set: {
+					img: updatedFigs.img,
+					email: updatedFigs.email,
+					name: updatedFigs.name,
+					price: updatedFigs.price,
+					quantity: updatedFigs.quantity,
+					seller: updatedFigs.seller,
+					description: updatedFigs.description,
+					Manufacturer: updatedFigs.Manufacturer,
+					category: updatedFigs.category,
+					rating: updatedFigs.rating,
+				},
+			};
+			const result = await addedFigureCollection.updateOne(filter, figures, options);
+			res.send(result);
+		});
+
+		app.delete("/addedFigure/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await addedFigureCollection.deleteOne(query);
+			res.send(result);
+		});
+
+		await client.db("admin").command({ ping: 1 });
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} finally {
+	}
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("animeFig server is running");
+	res.send("animeFig server is running");
 });
 
 app.listen(port, () => {
-    console.log(`animeFig server is running at ${port}`);
+	console.log(`animeFig server is running at ${port}`);
 });
